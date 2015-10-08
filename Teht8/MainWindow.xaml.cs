eeab2a7b731b7025f0e1f14615edb84e9d6bf6cf
pekka-m <propeke@gmail.com>
2015-10-08 17:34:48 +0300

    teht 8 check

---
+++ Teht8/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Teht8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (XMLWorker.NaytaPalautteet() == null)
+            {
+                MessageBox.Show(Properties.Settings.Default.PalauteXML + " tiedostoa ei löytynyt, luodaan uusi.");
+                // tiedostoa ei löytynyt tai ei kerennyt ladata tai jotain...
+                // tässä tehtäisiin uusi tiedosto
+                // XMLWorker.TeeXMLTiedosto();
+            }
+            else
+            {
+                datagrid_palautteet.ItemsSource = XMLWorker.NaytaPalautteet();
+            }
+        }
+
+        private void button_laheta_Click(object sender, RoutedEventArgs e)
+        {
+            XMLWorker.TallennaPalaute(textBox_nimi.Text, textBox_olen_oppinut.Text, textBox_haluan_oppia.Text, textBox_hyvaa.Text, textBox_parannettavaa.Text, textBox_muuta.Text);
+            datagrid_palautteet.ItemsSource = XMLWorker.NaytaPalautteet();
+        }
+    }
+}
---
+++ Teht8/XMLWorker.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Teht8
+{
+    public static class XMLWorker
+    {
+        public static void TeeXMLTiedosto()
+        {
+            string xmlFile = Properties.Settings.Default.PalauteXML;
+            XmlDocument doc = new XmlDocument();
+            XmlNode version = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            doc.AppendChild(version);
+            XmlNode rootNode = doc.CreateElement("palautteet");
+            doc.AppendChild(rootNode);
+            doc.Save(xmlFile);
+        }
+        public static void TallennaPalaute(string _tekija, string _opittu, string _haluanoppia, string _hyvaa, string _parannettavaa, string _muuta)
+        {
+            if (_tekija.Length != 0 && _opittu.Length != 0 && _haluanoppia.Length != 0 && _hyvaa.Length != 0 && _parannettavaa.Length != 0)
+            {
+                string xmlFile = Properties.Settings.Default.PalauteXML;
+                XmlDocument doc = new XmlDocument();
+
+                doc.Load(xmlFile);
+
+                XmlElement palaute = doc.CreateElement("palaute");
+                doc.DocumentElement.AppendChild(palaute);
+
+                XmlElement Xmlpvm = doc.CreateElement("pvm");
+                DateTime pvm = DateTime.Today.Date;
+                Xmlpvm.InnerText = pvm.ToString("yyyyMMdd");
+                palaute.AppendChild(Xmlpvm);
+
+                XmlElement tekija = doc.CreateElement("tekija");
+                tekija.InnerText = _tekija;
+                palaute.AppendChild(tekija);
+
+                XmlElement opittu = doc.CreateElement("opittu");
+                opittu.InnerText = _opittu;
+                palaute.AppendChild(opittu);
+
+                XmlElement haluanoppia = doc.CreateElement("haluanoppia");
+                haluanoppia.InnerText = _haluanoppia;
+                palaute.AppendChild(haluanoppia);
+
+                XmlElement hyvaa = doc.CreateElement("hyvaa");
+                hyvaa.InnerText = _hyvaa;
+                palaute.AppendChild(hyvaa);
+
+                XmlElement parannettavaa = doc.CreateElement("parannettavaa");
+                parannettavaa.InnerText = _parannettavaa;
+                palaute.AppendChild(parannettavaa);
+
+                XmlElement muuta = doc.CreateElement("muuta");
+                muuta.InnerText = _muuta;
+                palaute.AppendChild(muuta);
+                doc.Save(xmlFile);
+            }
+        }
+
+        public static System.ComponentModel.ICollectionView NaytaPalautteet()
+        {
+            string xmlFile = Properties.Settings.Default.PalauteXML;
+            try
+            {
+                XElement Palautteet = XElement.Load(xmlFile);
+                System.ComponentModel.ICollectionView c;
+                c = System.Windows.Data.CollectionViewSource.GetDefaultView(Palautteet.Elements());
+                return c;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
